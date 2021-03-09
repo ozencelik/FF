a90f3c2281f92e4a2a923bc9c72cadf9e7c444e0
Özenç Çelik <ozenc.celik@ceng.deu.edu.tr>
2021-03-09 14:42:49 +0300

    wwwroot folders added.

---
+++ src/FF.Web/wwwroot/js/demo/sidebar-scrollspy.demo.js
@@ -0,0 +1,13 @@
+/*
+Template Name: ASPSTUDIO - Responsive Bootstrap 4 Admin Template
+Version: 1.1.0
+Author: Sean Ngu
+Website: http://www.seantheme.com/asp-studio/
+*/
+
+$(document).ready(function () {
+    $('body').scrollspy({
+        target: "#sidebar-bootstrap",
+        offset: 200
+    })
+});
