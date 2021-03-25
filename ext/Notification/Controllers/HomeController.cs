3f42453b70659525da3dac5d75aacc6d3d41ee8a
ozenc.celik <ozenc.celik@ceng.deu.edu.tr>
2021-03-25 17:10:15 +0300

    Alert mechanism added as a different web project.

---
+++ ext/Notification/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Notification.Features.Alerts;
+using Notification.Models;
+using System.Diagnostics;
+
+namespace Notification.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly AlertService _alertService;
+
+        public HomeController(ILogger<HomeController> logger,
+            AlertService alertService)
+        {
+            _logger = logger;
+            _alertService = alertService;
+        }
+
+        public IActionResult Index()
+        {
+            _alertService.Success("Deneme success");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ext/Notification/ViewComponents/AlertViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Notification.Features.Alerts;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Notification.ViewComponents
+{
+    public class AlertViewComponent : ViewComponent
+    {
+        private readonly AlertService _alertService;
+
+        public AlertViewComponent(AlertService alertService)
+        {
+            _alertService = alertService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var alerts = _alertService.GetAlerts();
+
+            return await Task.FromResult((IViewComponentResult)View(alerts));
+        }
+    }
+}
